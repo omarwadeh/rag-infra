99969b2ca6c821cc8bdd93bd9f1e0d6baf4b7ebb
536351-Mazenalic-Feb22-Python101 <160591633+Mazenalic12@users.noreply.github.com>
2026-01-05 15:57:01 +0000

    Initial project files + automation

--- infrastructure/terraform/eks/main.tf
+++ infrastructure/terraform/eks/main.tf
@@ -3,7 +3,9 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      # Belangrijk: alleen provider 5.x gebruiken (6.x gaf die elastic_* errors)
+      version = ">= 5.0.0, < 6.0.0"
     }
   }
 }
@@ -12,7 +14,7 @@ provider "aws" {
   region = var.region
 }
 
-# Lees VPC state uit lokale state file van ../vpc
+# VPC state uit ../vpc (bestaat nu, want je hebt net apply gedaan)
 data "terraform_remote_state" "vpc" {
   backend = "local"
 
@@ -23,27 +25,44 @@ data "terraform_remote_state" "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "20.24.0"
 
-  # nieuwe namen in v21:
-  # cluster_name   -> name
-  # cluster_version -> kubernetes_version
-  name               = var.cluster_name
-  kubernetes_version = "1.29"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
 
   vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
   subnet_ids = data.terraform_remote_state.vpc.outputs.private_app_subnets
 
-eks_managed_node_groups = {
-  main = {
-    desired_size   = 1
-    min_size       = 1
-    max_size       = 1
-    instance_types = ["t3.small"]
-    capacity_type  = "ON_DEMAND"
-  }
-}
+create_kms_key = false
+cluster_encryption_config  = []
 
+create_cloudwatch_log_group = false
+cluster_enabled_log_types   = []
+
+
+
+cluster_endpoint_public_access          = true
+cluster_endpoint_private_access         = true
+cluster_endpoint_public_access_cidrs    = ["0.0.0.0/0"]
+
+enable_cluster_creator_admin_permissions = true
+
+eks_managed_node_groups = {}
+
+
+  fargate_profiles = {
+    default = {
+      name = "fp-default"
+
+      selectors = [
+        {
+          namespace = "default"
+        }
+      ]
+
+      subnet_ids = data.terraform_remote_state.vpc.outputs.private_app_subnets
+    }
+  }
 
   tags = {
     Project = "RAG"
--- infrastructure/terraform/eks/outputs.tf
+++ infrastructure/terraform/eks/outputs.tf
@@ -1,3 +1,14 @@
-output "cluster_name"  { value = module.eks.cluster_name }
-output "cluster_arn"   { value = module.eks.cluster_arn }
-output "oidc_provider" { value = module.eks.oidc_provider }
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = module.eks.cluster_arn
+}
+
+output "oidc_provider" {
+  description = "OIDC provider for the cluster"
+  value       = module.eks.oidc_provider
+}
